72d985c024ecc79a989fe3b1c2e37bf841a8a6ad
joydepdhar <joydepdhar138@gmai.com>
2023-10-09 10:29:40 +0600

    while loop done

---
+++ IntroTOC#/AllTypeOfLoop/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//While loop
+Console.WriteLine("Please Enter Your target: ");
+int UserTarget =int.Parse(Console.ReadLine());
+int start = 0;//loop start
+while(start <= UserTarget)//condition check
+{
+    Console.Write(start + " ");
+    start = start + 2;//never ending loop or infinity loop 
+}
--- IntroTOC#/IfAndSwitch/Program.cs
+++ IntroTOC#/IfAndSwitch/Program.cs
@@ -29,27 +29,61 @@
 // double && will check it will the check next condition
 // Difference between || and |?
 //swtich statement
-Console.WriteLine("Please enter a number: ");
-int UserName = int.Parse(Console.ReadLine());
-switch(UserName)
-{
-    //    case 10:
-    //      Console.WriteLine("Input 10");
-    //    break;
-    //case 20: Console.WriteLine("Input 20");
-    //  break;
-    //case 30:
-    //  Console.WriteLine("Input 20");
-    // break;
-    //default: Console.WriteLine("Wrong input");
-    //  break
-    case 10:
-    case 20:
-    case 30:
-        Console.WriteLine("Your number is {0}",UserName); break;   
-    default: Console.WriteLine("Your number is not 10 or 20 or 20");
-        break;
-;
-}
+//Console.WriteLine("Please enter a number: ");
+//int UserName = int.Parse(Console.ReadLine());
+//switch(UserName)
+//{
+//    case 10:
+//      Console.WriteLine("Input 10");
+//    break;
+//case 20: Console.WriteLine("Input 20");
+//  break;
+//case 30:
+//  Console.WriteLine("Input 20");
+// break;
+//default: Console.WriteLine("Wrong input");
+//  break
+//case 10:
+//case 20:
+//case 30:
+//  Console.WriteLine("Your number is {0}",UserName); break;   
+//default: Console.WriteLine("Your number is not 10 or 20 or 20");
+//  break;
+//;
+//}
 //break
 // go to statement
+
+int TotalCoffeeCost = 0;
+Start://level 
+Console.WriteLine("Please select Your Coffee Size:" +
+    " 1 - Small ,2 - Medium, 3 - Large");
+int UserChoice = int.Parse(Console.ReadLine());
+switch(UserChoice)
+{
+    case 1:
+        TotalCoffeeCost += 1; break;
+    case 2:
+        TotalCoffeeCost += 2; break;
+    case 3:
+        TotalCoffeeCost += 3; break;
+    default:
+        Console.WriteLine("Your Choice {0} is invalid", UserChoice);
+        goto Start;//break;//
+}
+Decide:
+Console.WriteLine("Do you want to buy another coffee - Yes or No");
+string UserDecision = Console.ReadLine();
+switch(UserDecision.ToUpper())
+{
+    case "YES":
+        goto Start;//to jump another statement// it should be avoided (bad programming) make program more complex
+    case "NO":
+        break;
+    default:
+        Console.WriteLine("Your Choice {0} is invalid. Please try again...",UserDecision);
+        goto Decide;
+}
+Console.WriteLine("Thank you for shopping with us");
+Console.WriteLine("Bill amount= {0}",TotalCoffeeCost);
+
