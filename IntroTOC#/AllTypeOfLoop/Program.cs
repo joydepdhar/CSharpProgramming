c470d65e09d4c4cdc90026c2d38e6dd831324c07
joydepdhar <joydepdhar138@gmai.com>
2023-10-09 23:44:43 +0600

    Untill method half complete

--- IntroTOC#/AllTypeOfLoop/Program.cs
+++ IntroTOC#/AllTypeOfLoop/Program.cs
@@ -1,10 +1,68 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //While loop
-Console.WriteLine("Please Enter Your target: ");
-int UserTarget =int.Parse(Console.ReadLine());
-int start = 0;//loop start
-while(start <= UserTarget)//condition check
-{
-    Console.Write(start + " ");
-    start = start + 2;//never ending loop or infinity loop 
-}
+//Console.WriteLine("Please Enter Your target: ");
+//int UserTarget =int.Parse(Console.ReadLine());
+//While loop demo:
+//int start = 0;//loop start
+//while(start <= UserTarget)//condition check
+//{
+//  Console.Write(start + " ");
+//start = start + 2;//never ending loop or infinity loop 
+//}
+// do while loop 
+//Console.WriteLine("Please enter your target number: ");
+//int usertarget = int.Parse(Console.ReadLine());
+//int start = 0;
+//do
+//{
+//  Console.WriteLine(start+2 + " ");
+//start++;
+//}
+//while (start < usertarget);
+//Console.WriteLine("Please Enter Your target? ");
+//int UserTarget = int.Parse(Console.ReadLine());
+//int Start = 0;
+//string UserChoice = "";
+//do
+//{
+//  while (Start <= UserTarget)
+//{
+//  Console.WriteLine(Start + " ");
+//Start = Start + 2;
+//}
+//string UserChoice = "";
+//do
+//{
+//  Console.WriteLine("Do you want to Continue - Yes or No ");
+//UserChoice = Console.ReadLine().ToUpper();
+//if (UserChoice != "YES" && UserChoice != "NO")
+//{
+//  Console.WriteLine("Invaild Choice, Please Say Yes or No");
+//}
+//} while (UserChoice != "NO" && UserChoice != "NO");
+//} while (UserChoice == "YES");
+
+//for each loop
+int[] Number = new int[3];
+Number[0] = 101;
+Number[1] = 102;
+Number[2] = 103;
+//int i = 0;
+//while(i < Number.Length)
+//{
+  //  Console.WriteLine(Number[i]);
+    //i++;
+//}
+//for(int j = 0; j < Number.Length; j++)
+//{
+  //  Console.WriteLine(Number[j]);
+//}
+//foreach (int k in Number)
+//{
+  //  Console.WriteLine(k);
+//}
+//break and continue should learn...'
+
+
+
+
---
+++ IntroTOC#/Methods/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//The structure of a method
+// the difference between static and instance methods
+//some knowledge about method
+//method are also called as functions. these terms are used interchangeably
+//Methods are extremely useful because they allow you to define your logic once and use it at many places
+//Methods make the maintenance of your application easier
+//structure of method
+//[attributes]
+// access-modifiers return type method-name (parameters)
+//{
+//method body
+//}
+// attribute and access modifiers  later
+//Return type can be any valid date type or void
+//method name can be any meaningfull name expect keyword
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program p = new Program();//method  invoke
+            p.EvenNumber();
+        }
+        public void EvenNumber()
+        {
+            int start = 0;
+            while (start <= 20)
+            {
+                Console.WriteLine(start);
+                start = start + 2;
+            }
+        }
+    }
+}
+
