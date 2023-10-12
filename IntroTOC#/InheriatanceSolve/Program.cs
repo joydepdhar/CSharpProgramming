03e7f40489faa095460895c57ed4d1e3cbb7256a
joydepdhar <joydepdhar138@gmai.com>
2023-10-12 23:18:10 +0600

    Inheriatance Completed

---
+++ IntroTOC#/InheriatanceSolve/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace InheriatanceSolve
+{
+    //public class Employee
+    //{
+    //    public string firstName;
+    //    public string lastName;
+    //    public string Email;
+    //    public void PrintFullName() 
+    //    {
+    //        //
+    //        Console.WriteLine("Full Name:{0} {1}",firstName,lastName);
+    //        Console.WriteLine("Email: {0}",Email);
+
+    //    }
+    //}
+    //public class FulltimeEmployee:Employee
+    //{
+    //    public float YearlySalary;
+    //}
+    //public class ParttimeEmployee:Employee
+    //{
+    //    public float HourlySalary;
+    //}
+    public class ParentClass
+    {
+        public ParentClass()
+        {
+            Console.WriteLine("Parent Class constructor");
+        }
+        public ParentClass(string msg) 
+        {
+            Console.WriteLine(msg);
+        }
+    }
+    public class ChildClass:ParentClass
+    {
+        public ChildClass():base("Derived class controlling Parent Class")
+        {
+            Console.WriteLine("Child Class constructor called");
+        }
+    }
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //FulltimeEmployee e = new FulltimeEmployee();
+            //e.firstName = "Joydep";
+            //e.lastName = "Dhar";
+            //e.YearlySalary = 100000;
+            //e.Email = "joydep.dhar@gmail.com";
+            //e.PrintFullName();
+            //ParttimeEmployee parttime = new ParttimeEmployee();
+            //parttime.firstName = "Joy";
+            //parttime.lastName = "Dep";
+            //parttime.Email = "Joy@gmail.com";
+            //parttime.HourlySalary = 20;
+            //parttime.PrintFullName();
+            //Console.WriteLine("Hello World");
+            ChildClass x= new ChildClass();
+            //ChildClass y= new ChildClass();
+        }
+    }
+}
+//note:
+//In this example Derived class inherits from parentClass
+//C# supports only single class inheritance
+//C# support multiple interface inheritance
+//child class is a specialization of base class
+//base classes are automatically instantiated before derived classes
+//parent class constructor executes before child class constructor 
--- IntroTOC#/StaticAndInstanceClassMembers/Program.cs
+++ IntroTOC#/StaticAndInstanceClassMembers/Program.cs
@@ -2,16 +2,34 @@
 {
     class Circle
     {
-        float pi = 3.141f;
+    //  float pi = 3.141f;
+        //static float pi = 3.141f;
+         public static float pi = 3.141f;
         int _Radius;
+        static Circle()
+        {
+            //by default it is private
+            //static constructor
+            //why i need this ?
+            //to initialize static field
+            Circle.pi = 3.1416f;
+            Console.WriteLine("Static Constructor Called");
+
+        }
         public Circle(int radius)
         {
+            Console.WriteLine("Instance constructor called");
             this._Radius = radius;
+        }
+        public static void print()
+        {
+            //
         }
         public float CalculateAreaMethod() 
         {
 
-            return this.pi*this._Radius*this._Radius;
+            //return this.pi*this._Radius*this._Radius;
+            return Circle.pi*this._Radius*this._Radius;
 
         }
     }
@@ -20,12 +38,24 @@
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");
-            Circle c = new Circle(5);
-            float Area1 =c.CalculateAreaMethod();
-            Console.WriteLine("Area1: {0}",Area1);
-            Circle c2 = new Circle(6);
-            float Area2 =c2.CalculateAreaMethod();
-            Console.WriteLine("Area2: {0}",Area2);
+            //Circle c = new Circle(5);
+            //float Area1 =c.CalculateAreaMethod();
+            //Console.WriteLine("Area1: {0}",Area1);
+            //Circle c2 = new Circle(6);
+            //float Area2 =c2.CalculateAreaMethod();
+            //Console.WriteLine("Area2: {0}",Area2);
+            //Circle.print();
+            Console.WriteLine(Circle.pi);
         }
     }
 }
+//some note :
+//when a class member includes a static modifier, the member is called as static member
+//when no static modifirer is present the member is called as non-static member or instance member
+//Static members are invoker using class name, where as instance members are invoked using instances (object) of the class.
+//A instance member belongs to specific instance(object) of a class. If I create 3 objects of a class I will have 3 sets of instance members in the memory where as there will ever be only one copy of a static member, no matter how many instance of a class are created
+//**Class member -- fields, methods, properties, events, indexers, constructors
+//static Constructor:Static Constructor are used to initialize static fields in a class
+//you declare a static constructor by using the keyword static in front of the constructor 
+//Static constructor is called only once no matter how many instances you create
+//Static Constructor are called before instance constructors
